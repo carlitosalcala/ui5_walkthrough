eca4bf0971e2769236211a29fdb6116f23e228fd
Carlos PI <carlosh@duck.com>
2022-06-05 17:26:29 +0200

    Shell Control as Container

--- webapp/index.js
+++ webapp/index.js
@@ -1,5 +1,10 @@
 sap.ui.getCore().attachInit(function () {
-  new sap.ui.core.ComponentContainer({
-    name: "sap.ui.demo.walkthrough",
+  new sap.m.Shell({
+    app: new sap.ui.core.ComponentContainer({
+      name: "sap.ui.demo.walkthrough",
+      settings: {
+        id: "walkthrough",
+      },
+    }),
   }).placeAt("content");
 });
